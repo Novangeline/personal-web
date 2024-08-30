663d652742558dc747fd7d8f7dd594d72097206b
Novangeline <nsan0027@student.monash.edu>
2024-08-31 00:25:00 +1000

    Update readme and changes on server.js
    Updated the readme and installed packages chalk

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # personal-web
  Personal Website
+
+How to run?
+
+Open terminal
+Run "npm i"
+Run "nodemon server.js" to run the web 
+Press the link on the terminal
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const ejs = require('ejs')
+const chalk = require('chalk');
 let app = express();
 const PORT_NUMBER = 8088;
-app.listen(PORT_NUMBER);
 
-app.engine("html", require("ejs").renderFile);
+app.engine("html", ejs.renderFile);
 app.set("view engine", "html");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("node_modules/bootstrap/dist/css"));
 app.use(express.static("images"));
+
+app.listen(PORT_NUMBER, function (req, res) {
+    console.log(`Server is running on ${chalk.blue(`http://localhost:${PORT_NUMBER}`)}`)
+});
 
 app.get("/", function (req, res) {
     res.render("index.html");
